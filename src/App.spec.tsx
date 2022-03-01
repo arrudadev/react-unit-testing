75605f230591829b3c1be5165a7c49712fb0712d
Alexandre <alexandre.monteiro.bec@gmail.com>
2022-03-01 12:23:32 -0300

    test: add test to add item to list

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from './App';
 
@@ -10,5 +11,15 @@ describe('App Component', () => {
     expect(getByText('Diego')).toBeInTheDocument();
     expect(getByText('Mayk')).toBeInTheDocument();
     expect(getByText('Rodz')).toBeInTheDocument();
+  });
+
+  it('should be able to add new item to the list', () => {
+    const { getByText } = render(<App />);
+
+    const addButton = getByText('Adicionar');
+
+    userEvent.click(addButton);
+
+    expect(getByText('Novo')).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import { useState } from 'react';
 function App() {
   const [list, setList] = useState(['Alexandre', 'Diego', 'Mayk', 'Rodz']);
 
+  function addItemInTheList() {
+    setList([...list, 'Novo']);
+  }
+
   return (
-    <ul>
-      {list.map(item => (
-        <li key={item}>{item}</li>
-      ))}
-    </ul>
+    <>
+      <button type="button" onClick={addItemInTheList}>
+        Adicionar
+      </button>
+
+      <ul>
+        {list.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
